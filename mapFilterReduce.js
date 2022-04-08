8f2c76520918f5004a9eb682f9290452a7c8365b
nassahAmer <hassan.amer.rr@gmail.com>
2022-04-08 13:28:50 -0400

    Adição de Filter com exemplo String

--- mapFilterReduce.js
+++ mapFilterReduce.js
@@ -6,12 +6,25 @@ const meuArray = [1, 2, 3, 4, 5];
 
 try {
     const multiplicado = meuArray.map((num) => num * 2);
-    console.log('Números multiplicados: ' + multiplicado);
+    console.log('Números multiplicados por 2: ' + multiplicado + "\n");
 } catch(e) {
     console.log(e.message);
 }
 
 // COM .this
 try {
-    const comThis = meuArray.map(function(num){ console.log(num * this)}, 3)
-} catch(e){ console.log(e.message)}
+    console.log('Números multiplicados por 3:');
+    const comThis = meuArray.map(function(num){ console.log(num * this)}, 3);
+    console.log("**********************************************");
+} catch(e){ console.log(e.message);}
+
+// FILTER
+// Filtre e retorne todos os números pares de um array.
+const animais = ["Gato", "Cachorro", "Gato Branco", "Cachorro Branco", "Papagaio Branco"];
+
+try {
+    const animalBranco = animais.filter((animal) => animal.includes("Branco"));
+    const cachorro = animais.filter((animal) => animal.includes("Cachorro"));
+    console.log('Apenas animais cor branca: ' + animalBranco);
+    console.log('Apenas cachoros: ' + cachorro);
+} catch(e) { console.log(e.message);}
