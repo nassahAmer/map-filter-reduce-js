7d4d65f733faa20d8a145226cdde177fed78c40e
nassahAmer <hassan.amer.rr@gmail.com>
2022-04-08 13:05:46 -0400

    Adição de exemplo MAP utilizando parâmetro this

--- mapFilterReduce.js
+++ mapFilterReduce.js
@@ -1,11 +1,17 @@
 // MAP
 // Pratique a sintaxe de multiplicação de números, uma vez utilizando o parâmetro this de um objeto criado por você, e depois sem ele
 
+// SEM .this
 const meuArray = [1, 2, 3, 4, 5];
 
 try {
     const multiplicado = meuArray.map((num) => num * 2);
-    console.log(multiplicado);
+    console.log('Números multiplicados: ' + multiplicado);
 } catch(e) {
     console.log(e.message);
 }
+
+// COM .this
+try {
+    const comThis = meuArray.map(function(num){ console.log(num * this)}, 3)
+} catch(e){ console.log(e.message)}
